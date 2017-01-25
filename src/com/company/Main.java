e825020aaf4b45916583f5c77b1924e5f838faec
jsmith0 <justins4@comcast.net>
2017-01-24 21:09:47 -0500

    OMG, this is tough....

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,39 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner kb = new Scanner (System.in);
-        System.out.println("Enter the type of the color you want, \"name\" \"RGB\" or \"hex\"");
-        String style = kb.next();
 
-        if (style.equalsIgnoreCase("name")){
-            System.out.println("Enter the name of your color.");
-            String nm = kb.next();
-        }
-        if (style.equalsIgnoreCase("rgb")){
 
-        }
-        if (style.equalsIgnoreCase("hex")){
+        while (true) {
+            System.out.println("Enter the type of the color you want, \"name\" \"RGB\" \"hex\" or \"quit\"");
+            String style = kb.next();
 
+            if (style.equalsIgnoreCase("name")) {
+                System.out.println("Enter your color.");
+                String SoConfused = kb.next();
+
+                CommonName.GetCommonName(SoConfused);
+                break;
+            }
+            else if (style.equalsIgnoreCase("rgb")) {
+                System.out.println("Enter your color.");
+                String SoConfused = kb.next();
+
+                CommonName.GetCommonName(SoConfused);
+                break;
+            }
+            else if (style.equalsIgnoreCase("hex")) {
+                System.out.println("Enter your color.");
+                String SoConfused = kb.next();
+
+                CommonName.GetCommonName(SoConfused);
+                break;
+            }
+            else if (style.equalsIgnoreCase("quit")){
+                break;
+            }
+            else {
+                System.out.println("Try again... Or type \"quit\"");
+            }
         }
     }
 }
